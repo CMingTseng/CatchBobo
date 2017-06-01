03d5021d43bdbd02b5c536ce3ee3bd06ee4d45e5
Neo <mingtseng.chang@huafu.com.tw>
2017-06-02 01:12:59 +0800

    add Config

---
+++ app/src/main/java/c/min/tseng/C.java
@@ -0,0 +1,12 @@
+package c.min.tseng;
+
+/**
+ * Created by Neo on 2017/5/31.
+ */
+
+public class C {
+    public static final String ACCOUNT_NAME = "account_name";
+    public static final String DATETIMEFORMAT = "yyyy-MM-dd HH:mm:ss";
+    public static final String PHOTO_PATH = "car_photo";
+    public static final String OCR_PATH = "start_ocr";
+}
