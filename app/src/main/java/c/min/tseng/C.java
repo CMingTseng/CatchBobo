16196e748881c758f3b9714704c07a0525865693
Neo <mingtseng.chang@huafu.com.tw>
2017-06-03 19:10:22 +0800

    add Date Config

--- app/src/main/java/c/min/tseng/C.java
+++ app/src/main/java/c/min/tseng/C.java
@@ -7,6 +7,7 @@ package c.min.tseng;
 public class C {
     public static final String ACCOUNT_NAME = "account_name";
     public static final String DATETIMEFORMAT = "yyyy-MM-dd HH:mm:ss";
+    public static final String DATEFORMAT = "yyyy-MM-dd";
     public static final String PHOTO_PATH = "car_photo";
     public static final String OCR_PATH = "start_ocr";
 }
